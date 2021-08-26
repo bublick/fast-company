63e39ca6712750c81a69b691259b61a9f19051cf
Kirill <k@lis.im>
2021-08-26 08:27:17 +0300

    Intro task complete

--- src/Components/users.jsx
+++ src/Components/users.jsx
@@ -1,9 +1,86 @@
-import React from "react";
-import api from "../API";
+import React, { useState } from "react";
+import api from "../api";
 
 const Users = () => {
-  console.log(api.users.fetchAll());
-  return <h1>Users</h1>;
+  const [users, setUsers] = useState(api.users.fetchAll());
+
+  const handleUserRemove = (id) => {
+    setUsers(users.filter((user) => user._id !== id));
+  };
+
+  const renderPhrase = (number) => {
+    let phraseClasses = "badge m-1 bg-";
+    if (number === 1 || number >= 5) {
+      phraseClasses += "primary";
+      let phrase = number + " человек тусанет с тобой сегодня";
+      return <div className={phraseClasses}>{phrase}</div>;
+    } else if (number > 1 && number < 5) {
+      phraseClasses += "primary";
+      let phrase = number + " человека тусанет с тобой сегодня";
+      return <div className={phraseClasses}>{phrase}</div>;
+    } else {
+      phraseClasses += "danger";
+      let phrase = "Никто с тобой не тусанет";
+      return <div className={phraseClasses}>{phrase}</div>;
+    }
+  };
+
+  return (
+    <>
+      <h2>{renderPhrase(users.length)}</h2>
+      {users.length == false ? (
+        ""
+      ) : (
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретились, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">Действие</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => {
+                return (
+                  <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>
+                      <ul className="p-0">
+                        {user.qualities.map((quality) => {
+                          let qualityClass = "badge m-1 bg-";
+                          qualityClass += quality.color;
+                          return (
+                            <li className={qualityClass} key={quality._id}>
+                              {quality.name}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate} / 5</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleUserRemove(user._id)}
+                      >
+                        delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Users;
