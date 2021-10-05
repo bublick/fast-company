8469905c49f22d01a641bc6d73efe50d3e167b33
bublick <k@lis.im>
2021-10-05 10:28:51 +0300

    Fix for lesson 6

--- src/Components/tableBody.jsx
+++ src/Components/tableBody.jsx
@@ -18,13 +18,17 @@ const TableBody = ({ data, columns }) => {
 
     return (
         <tbody>
-            {data.map((item) => (
-                <tr key={item._id}>
-                    {Object.keys(columns).map((column) => (
-                        <td key={column}>{renderContent(item, column)}</td>
-                    ))}
-                </tr>
-            ))}
+            {data.length
+                ? data.map((item) => (
+                      <tr key={item._id}>
+                          {Object.keys(columns).map((column) => (
+                              <td key={column}>
+                                  {renderContent(item, column)}
+                              </td>
+                          ))}{" "}
+                      </tr>
+                  ))
+                : null}
         </tbody>
     );
 };
