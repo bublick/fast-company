9b75eec50c8be00375b5cfcbc7dc54fb1b6154c5
bublick <k@lis.im>
2021-10-19 14:18:41 +0300

    Update

---
+++ src/Components/textField.jsx
@@ -0,0 +1,64 @@
+import { React, useState } from "react";
+import PropTypes from "prop-types";
+import { iconEye, iconEyeSlash } from "../utils/icons";
+
+const TextField = ({
+    label,
+    type,
+    name,
+    value,
+    onChange,
+    error,
+    placeholder
+}) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const getInputClasses = () => {
+        return "form-control" + (error ? " is-invalid" : "");
+    };
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
+    return (
+        <div className="mb-4">
+            <label htmlFor={name}>{label}</label>
+            <div className="input-group has-validation">
+                <input
+                    type={showPassword ? "text" : type}
+                    id={name}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    className={getInputClasses()}
+                />
+                {type === "password" && (
+                    <button
+                        className="btn btn-ouline-secondary"
+                        type="button"
+                        onClick={toggleShowPassword}
+                    >
+                        {showPassword ? iconEye : iconEyeSlash}
+                    </button>
+                )}
+                {error && <div className="invalid-feedback">{error}</div>}
+            </div>
+        </div>
+    );
+};
+
+TextField.defaultProps = {
+    type: "text"
+};
+
+TextField.propTypes = {
+    label: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    error: PropTypes.string,
+    placeholder: PropTypes.string
+};
+
+export default TextField;
--- src/Layouts/login.jsx
+++ src/Layouts/login.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TextField from "../Components/textField";
+import { validator } from "../utils/validator";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Почта введена неверно"
+            }
+        },
+        password: {
+            isRequired: {
+                message: "Поле пароль обязательно для заполнения"
+            },
+            isCapitalSymbol: {
+                message: "Поле должно содержать хотябы одну заглавнуб букву"
+            },
+            isContainDigit: {
+                message: "Поле должно содержать хотябы одну цифру"
+            },
+            min: {
+                message: "Поле должно содержать минимум 8 символов",
+                value: 8
+            }
+        }
+    };
+
+    useEffect(() => {
+        validate();
+    }, [data]);
+
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+
+        for (const fieldName in data) {
+            if (data[fieldName].trim() === "") {
+                errors[fieldName] = `${fieldName} обязательно для заполннения`;
+            }
+        }
+
+        setErrors(errors);
+        return Object.keys(errors).length !== 0;
+    };
+
+    const isValid = Object.keys(errors).length !== 0;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (isValid) return;
+        console.log(data);
+    };
+
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Email"
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-primary w-100 mx-auto"
+                            disabled={isValid}
+                        >
+                            Отправить
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
